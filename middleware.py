b361ae0a19aaf8d11c1913b03228f702a4049910
Karthik <karthikk1162@gmail.com>
2026-03-10 12:23:13 +0530

    For logging, authentication, request timing, and metrics

---
+++ middleware.py
@@ -0,0 +1,24 @@
+import time
+from logger import log_info
+
+
+class MiddlewareManager:
+
+    def __init__(self):
+        self.middlewares = []
+
+    def add(self, middleware_func):
+        self.middlewares.append(middleware_func)
+
+    def run_before(self, request):
+        for m in self.middlewares:
+            m(request)
+
+
+##Example middleware
+def logging_middleware(request):
+    log_info(f"[MIDDLEWARE] {request.method} {request.path}")
+
+
+def timing_middleware(request):
+    request.start_time = time.time()
